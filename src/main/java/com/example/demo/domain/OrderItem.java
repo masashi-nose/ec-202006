4fe0ab601f714c20f8c68a0325c49dbed093f3f2
nosemasashi <ma.nose9413@gmail.com>
2020-06-04 19:53:52 +0900

    OrderItemドメインを作成しました。

---
+++ src/main/java/com/example/demo/domain/OrderItem.java
@@ -0,0 +1,94 @@
+package com.example.demo.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 
+ * order_itemsテーブルのエンティティ.
+ * 
+ * @author masashi.nose
+ *
+ */
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "item_id")
+	private Integer itemId;
+	
+	@Column(name = "order_id")
+	private Integer orderId;
+	
+	@Column(name = "quantity")
+	private Integer quantity;
+	
+	private Item item;
+
+	/**
+	 * 
+	 * 商品の小計
+	 * 
+	 * @return
+	 */
+	public Integer getSubTotal() {
+		Integer subTotal = item.getPrice() * quantity;
+		return subTotal;
+
+	}
+
+	@Override
+	public String toString() {
+		return "OrderItem [id=" + id + ", itemId=" + itemId + ", orderId=" + orderId + ", quantity=" + quantity
+				+ ", item=" + item + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+
+	public Integer getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+}
