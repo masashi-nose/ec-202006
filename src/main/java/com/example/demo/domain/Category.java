6c4293f2d40a4740e7a93659dd6eab6439c8326d
nosemasashi <ma.nose9413@gmail.com>
2020-06-04 17:04:17 +0900

    Categoryドメインを作成しました。

---
+++ src/main/java/com/example/demo/domain/Category.java
@@ -0,0 +1,49 @@
+package com.example.demo.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 
+ * categoriesテーブルのエンティティ.
+ * 
+ * @author masashi.nose
+ *
+ */
+@Entity
+@Table(name = "categories")
+public class Category {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "category_name")
+	private String categoryName;
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", categoryName=" + categoryName + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+}
