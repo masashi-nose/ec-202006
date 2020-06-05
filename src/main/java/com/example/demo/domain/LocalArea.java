744b2bb80fcbcce6eb2a25fea588bf4841e5f6db
nosemasashi <ma.nose9413@gmail.com>
2020-06-05 13:31:45 +0900

    LocalAreaテーブルを作成しました。

---
+++ src/main/java/com/example/demo/domain/LocalArea.java
@@ -0,0 +1,48 @@
+package com.example.demo.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * local_areasテーブルのエンティティ.
+ * 
+ * @author masashi.nose
+ *
+ */
+@Entity
+@Table(name = "local_areas")
+public class LocalArea {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "local_area_name")
+	private String localAreaName;
+
+	@Override
+	public String toString() {
+		return "LocalArea [id=" + id + ", localAreaName=" + localAreaName + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLocalAreaName() {
+		return localAreaName;
+	}
+
+	public void setLocalAreaName(String localAreaName) {
+		this.localAreaName = localAreaName;
+	}
+
+}
