797d4aaf3aaed10f680ed76f50fe68d87e4f562c
nosemasashi <ma.nose9413@gmail.com>
2020-06-10 17:31:03 +0900

    SowItemDetailControllerを作成しました。

---
+++ src/main/java/com/example/demo/controller/ShowItemDetailController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.domain.Item;
+import com.example.demo.service.ShowItemDetailService;
+
+/**
+ * 商品詳細画面表示を操作するコントローラクラス.
+ * 
+ * @author masashi.nose
+ *
+ */
+@Controller
+@RequestMapping("")
+public class ShowItemDetailController {
+
+	@Autowired
+	private ShowItemDetailService showItemDetailService;
+
+	/**
+	 * 商品詳細画面へ遷移します.
+	 * 
+	 * @param id
+	 * @param model
+	 * @return
+	 */
+	public String showDetail(Integer id, Model model) {
+		Optional<Item> item = showItemDetailService.showDetail(id);
+
+		if (item.isPresent()) {
+			model.addAttribute("item", item);
+			return "detail";
+
+		} else
+			return null;
+
+	}
+
+}
