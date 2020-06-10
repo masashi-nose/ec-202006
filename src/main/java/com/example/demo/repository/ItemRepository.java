4bf848fe2939299a1cc5a3908989cdd872d15e48
nosemasashi <ma.nose9413@gmail.com>
2020-06-10 16:03:57 +0900

    list.htmlを編集しました。

--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -1,6 +1,11 @@
 package com.example.demo.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.Item;
 
@@ -10,6 +15,22 @@ import com.example.demo.domain.Item;
  * @author masashi.nose
  *
  */
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+	/**
+	 * カテゴリーIDから商品を検索します
+	 * 
+	 * @param categoryId
+	 * @return
+	 */
+	List<Item> findByCategoryId(Integer categoryId);
+
+	/**
+	 * IDから商品を検索します.
+	 *
+	 */
+	@Override
+	Optional<Item> findById(Integer id);
 
 }
