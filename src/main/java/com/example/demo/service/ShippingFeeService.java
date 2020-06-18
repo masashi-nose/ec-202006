a46898ecb2738056beba0a58e30c216f3aab0061
nosemasashi <ma.nose9413@gmail.com>
2020-06-18 14:44:52 +0900

    ShippingFeeServiceを作成しました。

---
+++ src/main/java/com/example/demo/service/ShippingFeeService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.ShippingFee;
+import com.example.demo.repository.ShippingFeeRepository;
+
+@Service
+@Transactional
+public class ShippingFeeService {
+
+	@Autowired
+	private ShippingFeeRepository shippingFeeRepository;
+	
+	public List<ShippingFee> showAllShippingFee(){
+		return shippingFeeRepository.findAll();
+	}
+}
